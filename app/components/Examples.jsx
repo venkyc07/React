dfdf8be09f1a8aa4beb3a5d226603c25c414019f
penguin <venky@penguincrm.com>
2016-08-29 09:59:37 +0530

    Initial commit

---
+++ app/components/Examples.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+// var Examples = React.createClass({
+//   render: function () {
+//     return (
+//        <h3>Example component</h3>
+//     );
+//   }
+// });
+
+var Examples = (props) => {
+  return (
+     <h3>Example component</h3>
+  );
+};
+
+module.exports = Examples;
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+
+// var WeatherMessage = React.createClass({
+//   render: function () {
+//     var {temp, location} = this.props;
+//
+//     return (
+//        <h3>It's it {temp} in {location}</h3>
+//     );
+//   }
+// });
+
+//var WeatherMessage = (props) => {
+var WeatherMessage = ({temp, location}) => {
+  //var {temp, location} = props;
+
+  return (
+     <h3>It's it {temp} in {location}</h3>
+  );
+};
+
+module.exports = WeatherMessage;
